accbd785775ce8540d241c918ce34ac749137b84
Bruno Pavlović <bpavlovic20@foi.hr>
2024-06-23 02:30:27 +0200

    Deleted generated classes and created Product model

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Middleware.Models
+{
+    public class Product
+    {
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private double Price { get; set; }
+        private string Category { get; set; }
+        private string Thumbnail { get; set; }
+    }
+}
