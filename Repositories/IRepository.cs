ff69020d933ff9c2c4a549c8c3f4c4bb82d5d1ef
Bruno Pavlović <bpavlovic20@foi.hr>
2024-06-23 02:35:57 +0200

    Created generic repository interface

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Middleware.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
